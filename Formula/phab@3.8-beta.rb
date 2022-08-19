91aef592b1576022113d2ecb30389cf9b8f88f23
Stephan Maximilian Huber <stephan@factorial.io>
2022-08-19 18:03:14 +0200

    Update phab@3.8-beta.rb

--- Formula/phab@3.8-beta.rb
+++ Formula/phab@3.8-beta.rb
@@ -1,8 +1,8 @@
 class PhabAT38Beta < Formula
   desc "Supports your deployments and every-day devops-tasks "
   homepage "http://docs.phab.io"
-  url "https://github.com/factorial-io/phabalicious/releases/download/3.8.0-beta.16/phabalicious.phar"
-  sha256 "49fe74d22f1c1547708aeb1d722b288e4797afeb7f1baa144e345210475416c2"
+  url "https://github.com/factorial-io/phabalicious/releases/download/3.8.0-beta.17/phabalicious.phar"
+  sha256 "185ef73446d4815cdb7a8b836975c6b7d0da3d8f4d0dd9c4d7a50cb93dcf7e76"
   license "MIT"
   def install
     bin.install 'phabalicious.phar' => 'phab'
