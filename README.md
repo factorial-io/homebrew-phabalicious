a8485aaa84a9c25000778b8adc3442b191200fb1
D34dMan <shibin@factorial.io>
2022-08-19 09:53:37 +0200

    Add 3.7.18

---
+++ README.md
@@ -0,0 +1,8 @@
+# Homebrew Phabalicious
+
+[Phabalicious](https://docs.phab.io/) formulae for the Homebrew package manager.
+
+```
+brew tap factorial-io/homebrew-phabalicious
+brew install phabalicious
+```
---
+++ phabalicious.rb
@@ -0,0 +1,11 @@
+class Phabalicious < Formula
+  desc "Supports your deployments and every-day devops-tasks "
+  homepage "http://docs.phab.io"
+  url "https://github.com/factorial-io/phabalicious/releases/download/3.7.18/phabalicious.phar"
+  sha256 "441aba02d75eb0ec2a72dde15682fab8bc5dd88ac3a6de9d0a3cf3b484083d80"
+  license "MIT"
+  depends_on "box"
+  def install
+    bin.install 'phabalicious.phar' => 'phab'
+  end
+end
