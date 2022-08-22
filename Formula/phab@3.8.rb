1aeeb6e5a69e3980860e0ea97a7269fe3b56d798
Stephan Huber <stephan@factorial.io>
2022-08-22 11:31:26 +0200

    fix: class for 3.8

--- Formula/phab@3.8.rb
+++ Formula/phab@3.8.rb
@@ -1,4 +1,4 @@
-class PhabaliciousAt3Point8 < Formula
+class PhabAT38 < Formula
   desc " Supports your deployments and every-day devops-tasks "
   homepage "http://docs.phab.io"
   url "https://github.com/factorial-io/phabalicious/releases/download/3.8.0/phabalicious.phar"
