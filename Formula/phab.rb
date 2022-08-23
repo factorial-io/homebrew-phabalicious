59030a2c0d8815e1b908bb9bb28cdf141fae4a42
Stephan Maximilian Huber <stephan@factorial.io>
2022-08-23 14:09:41 +0200

    Update phab.rb

--- Formula/phab.rb
+++ Formula/phab.rb
@@ -1,8 +1,8 @@
 class Phab < Formula
   desc " Supports your deployments and every-day devops-tasks "
   homepage "http://docs.phab.io"
-  url "https://github.com/factorial-io/phabalicious/releases/download/3.8.0/phabalicious.phar"
-  sha256 "ccca1157883d0d330f8c629025de919bfd1ddab3ae56da7f7040af20c73d149f"
+  url "https://github.com/factorial-io/phabalicious/releases/download/3.8.3/phabalicious.phar"
+  sha256 "200a39b6f34aff20b7b965bf4f24ce85c48b6be2ca9886923fa9075e7399b6a9"
   license "MIT"
   def install
     bin.install 'phabalicious.phar' => 'phab'
